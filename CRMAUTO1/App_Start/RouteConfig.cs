f90868b70deb2f855b19c2d9e5e2d94cfcd62e29
John Doe <mouadrms8@gmail.com>
2022-04-04 19:01:19 +0100

    hey

--- CRMAUTO1/App_Start/RouteConfig.cs
+++ CRMAUTO1/App_Start/RouteConfig.cs
@@ -12,7 +12,7 @@ namespace CRMAUTO1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "Register", id = UrlParameter.Optional }
             );
         }
     }
